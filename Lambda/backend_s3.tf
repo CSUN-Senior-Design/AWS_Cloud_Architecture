419e3895fe9c539358088faafa7d74c9f946db38
Petra Antyanti Larisa Anggraeni <petraantyantilarisa.anggraeni.154@my.csun.edu>
2020-02-27 13:47:58 -0800

    added comments
    please read the comments before running it

--- Lambda/backend_s3.tf
+++ Lambda/backend_s3.tf
@@ -1,5 +1,8 @@
 terraform {
   backend "s3" {
+    #put the bucket name
+    #my bucket name is storage.solution.s3
+    #modify accordingly
     bucket = "storage.solution.s3"
     key = "terraform.tfstate"
     region = "us-east-1"
